8ad1042931af8b415b36ad9a2116abfe65d96a8b
harikumar69 <115579391+harikumar69@users.noreply.github.com>
2025-01-06 12:10:21 +0545

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (async () => {
     // Fetch the DOM content of /bank/main.jsp
-    const response = await fetch('/bank/main.jsp', {
+    const response = await fetch('/discounts/list_discounts', {
         method: 'GET',
         credentials: 'include', // Include cookies for authenticated requests
     });
